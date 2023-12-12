6ab950b5ba56e5dc84034da98991a14669a714ab
Joseph Dorman <JDorman555@yahoo.com>
2023-12-11 20:39:36 -0600

    command to join vc

---
+++ src/commands/join.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, ChannelType } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("join")
+    .setDescription("Joins a Voice Channel")
+    .addChannelOption((option) =>
+      option
+        .setName("channel")
+        .setDescription("The channel to join")
+        .setRequired(true)
+        .addChannelTypes(ChannelType.GuildVoice)
+    ),
+  async execute(interaction) {
+    const voiceChannel = interaction.options.getChannel("channel");
+    const voiceConnection = await joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: interaction.guild.id,
+      adapterCreator: interaction.guild.voiceAdapterCreator,
+    });
+
+    await interaction.reply(`Joined ${voiceChannel}`);
+    console.log(voiceConnection);
+  },
+};
