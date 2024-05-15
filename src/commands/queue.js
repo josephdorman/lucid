8b284d7e308ebbe37aab225854ea33bcdf296149
Joseph Dorman <JDorman555@yahoo.com>
2024-05-14 19:48:33 -0500

    feat: command to view current queue

---
+++ src/commands/queue.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("queue")
+    .setDescription("Shows the current queue"),
+  async execute(interaction) {
+    try {
+      const queue = interaction.client.distube.getQueue(interaction);
+
+      if (!queue) {
+        return await interaction.reply("There is no current queue.");
+      }
+
+      // .name, .sourve .formattedDuration, .thumbnail, .url are a few of the properties to access
+      const q = queue.songs
+        .map(
+          (song, i) =>
+            `${i === 0 ? "Playing:" : `${i}.`} ${song.name} - \`${
+              song.formattedDuration
+            }\``
+        )
+        .join("\n");
+
+      console.log(q);
+
+      await interaction.reply(`**Server Queue** \n${q}`);
+    } catch (e) {
+      await interaction.reply("Something went wrong...");
+    }
+  },
+};
